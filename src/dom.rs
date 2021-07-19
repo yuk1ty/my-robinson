bdca28a73850fdbcfadb5c0dd17d79af8072c551
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-07-19 17:49:00 +0900

    first commit

---
+++ src/dom.rs
@@ -0,0 +1,54 @@
+use std::{
+    collections::{HashMap, HashSet},
+    str,
+};
+
+pub type AttrMap = HashMap<String, String>;
+
+#[derive(Debug)]
+pub struct Node {
+    pub children: Vec<Node>,
+    pub node_type: NodeType,
+}
+
+#[derive(Debug)]
+pub enum NodeType {
+    Element(ElementData),
+    Text(String),
+}
+
+#[derive(Debug)]
+pub struct ElementData {
+    pub tag_name: String,
+    pub attributes: AttrMap,
+}
+
+pub fn text(data: String) -> Node {
+    Node {
+        children: vec![],
+        node_type: NodeType::Text(data),
+    }
+}
+
+pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs,
+        }),
+    }
+}
+
+impl ElementData {
+    pub fn id(&self) -> Option<&String> {
+        self.attributes.get("id")
+    }
+
+    pub fn classes(&self) -> HashSet<&str> {
+        match self.attributes.get("class") {
+            Some(classlist) => classlist.split(' ').collect(),
+            None => HashSet::new(),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod dom;
+
+fn main() {
+    println!("Hello, world!");
+}
