1937561a042f1098f788d2fd79544b098801cb31
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-07-30 22:42:22 +0900

    add style nodes

--- src/style.rs
+++ src/style.rs
@@ -1,11 +1,87 @@
 use std::collections::HashMap;
 
-use crate::{css::Value, dom::Node};
+use crate::{
+    css::{Rule, Selector, SimpleSelector, Specificity, Stylesheet, Value},
+    dom::{ElementData, Node, NodeType},
+};
 
 type PropertyMap = HashMap<String, Value>;
 
-struct StyledNode<'a> {
+pub struct StyledNode<'a> {
     node: &'a Node,
     specified_values: PropertyMap,
     children: Vec<StyledNode<'a>>,
+}
+
+pub fn style_tree<'a>(root: &'a Node, stylesheet: &'a Stylesheet) -> StyledNode<'a> {
+    StyledNode {
+        node: root,
+        specified_values: match root.node_type {
+            NodeType::Element(ref elem) => specified_values(elem, stylesheet),
+            NodeType::Text(_) => HashMap::new(),
+        },
+        children: root
+            .children
+            .iter()
+            .map(|child| style_tree(child, stylesheet))
+            .collect(),
+    }
+}
+
+fn matches(elem: &ElementData, selector: &Selector) -> bool {
+    match *selector {
+        Selector::Simple(ref simple_selector) => matches_simple_selector(elem, simple_selector),
+    }
+}
+
+type MatchedRule<'a> = (Specificity, &'a Rule);
+
+fn matching_rules<'a>(elem: &ElementData, stylesheet: &'a Stylesheet) -> Vec<MatchedRule<'a>> {
+    stylesheet
+        .rules
+        .iter()
+        .filter_map(|rule| match_rule(elem, rule))
+        .collect()
+}
+
+fn match_rule<'a>(elem: &ElementData, rule: &'a Rule) -> Option<MatchedRule<'a>> {
+    rule.selectors
+        .iter()
+        .find(|selector| matches(elem, *selector))
+        .map(|selector| (selector.specificity(), rule))
+}
+
+fn specified_values(elem: &ElementData, stylesheet: &Stylesheet) -> PropertyMap {
+    let mut values = HashMap::new();
+    let mut rules = matching_rules(elem, stylesheet);
+
+    rules.sort_by(|&(a, _), &(b, _)| a.cmp(&b));
+    for (_, rule) in rules {
+        for declaration in &rule.declarations {
+            values.insert(declaration.name.clone(), declaration.value.clone());
+        }
+    }
+
+    values
+}
+
+fn matches_simple_selector(elem: &ElementData, selector: &SimpleSelector) -> bool {
+    if selector.tag_name.iter().any(|name| elem.tag_name != *name) {
+        return false;
+    }
+
+    if selector.id.iter().any(|id| elem.id() != Some(id)) {
+        return false;
+    }
+
+    let elem_classes = elem.classes();
+    if selector
+        .class
+        .iter()
+        .any(|class| !elem_classes.contains(&**class))
+    {
+        return false;
+    }
+
+    true
 }
