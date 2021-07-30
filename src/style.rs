438e9bebe3eeb1052c92b8deebfcdc4c507ee2d0
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-07-30 22:06:59 +0900

    add basic data structure

--- src/style.rs
+++ src/style.rs
@@ -1,1 +1,11 @@
+use std::collections::HashMap;
 
+use crate::{css::Value, dom::Node};
+
+type PropertyMap = HashMap<String, Value>;
+
+struct StyledNode<'a> {
+    node: &'a Node,
+    specified_values: PropertyMap,
+    children: Vec<StyledNode<'a>>,
+}
