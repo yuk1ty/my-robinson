e9ed59ff2a4137349f379a91803485e409be2fb3
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-08-08 00:08:13 +0900

    add layout phase

--- src/layout.rs
+++ src/layout.rs
@@ -1,6 +1,9 @@
-use crate::style::StyledNode;
+use crate::{
+    css::{Unit, Value},
+    style::StyledNode,
+};
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 struct Dimensions {
     content: Rect,
     padding: EdgeSizes,
@@ -8,7 +11,21 @@ struct Dimensions {
     margin: EdgeSizes,
 }
 
-#[derive(Default)]
+impl Dimensions {
+    fn padding_box(self) -> Rect {
+        self.content.expanded_by(self.padding)
+    }
+
+    fn border_box(self) -> Rect {
+        self.content.expanded_by(self.border)
+    }
+
+    fn margin_box(self) -> Rect {
+        self.border_box().expanded_by(self.margin)
+    }
+}
+
+#[derive(Default, Clone, Copy)]
 struct Rect {
     x: f32,
     y: f32,
@@ -16,7 +33,18 @@ struct Rect {
     height: f32,
 }
 
-#[derive(Default)]
+impl Rect {
+    fn expanded_by(self, edge: EdgeSizes) -> Rect {
+        Rect {
+            x: self.x - edge.left,
+            y: self.y - edge.top,
+            width: self.width + edge.left + edge.right,
+            height: self.height + edge.top + edge.bottom,
+        }
+    }
+}
+
+#[derive(Default, Clone, Copy)]
 struct EdgeSizes {
     left: f32,
     right: f32,
@@ -36,6 +64,157 @@ impl<'a> LayoutBox<'a> {
             box_type: box_type,
             dimensions: Default::default(),
             children: Vec::new(),
+        }
+    }
+
+    fn layout(&mut self, containing_block: Dimensions) {
+        match self.box_type {
+            BoxType::BlockNode(_) => self.layout_block(containing_block),
+            BoxType::InlineNode(_) => {}
+            BoxType::AnonymousBlock => {}
+        }
+    }
+
+    fn layout_block(&mut self, containing_block: Dimensions) {
+        self.calculate_block_width(containing_block);
+        self.caluclate_block_position(containing_block);
+        self.layout_block_children();
+        self.calculate_block_height();
+    }
+
+    fn calculate_block_width(&mut self, containing_block: Dimensions) {
+        let style = self.get_style_node();
+        let auto = Value::Keyword("auto".to_string());
+        let mut width = style.value("width").unwrap_or(auto.clone());
+
+        let zero = Value::Length(0.0, Unit::Px);
+
+        let mut margin_left = style.lookup("margin-left", "margin", &zero);
+        let mut margin_right = style.lookup("margin-right", "margin", &zero);
+
+        let border_left = style.lookup("border-left", "border", &zero);
+        let border_right = style.lookup("border-right", "border", &zero);
+
+        let padding_left = style.lookup("padding-left", "padding", &zero);
+        let padding_right = style.lookup("padding-right", "padding", &zero);
+
+        let total: f32 = [
+            &margin_left,
+            &margin_right,
+            &border_left,
+            &border_right,
+            &padding_left,
+            &padding_right,
+            &width,
+        ]
+        .iter()
+        .map(|v| v.to_px())
+        .sum();
+
+        if width != auto && total > containing_block.content.width {
+            if margin_left == auto {
+                margin_left = Value::Length(0.0, Unit::Px);
+            }
+            if margin_right == auto {
+                margin_right = Value::Length(0.0, Unit::Px);
+            }
+        }
+
+        let underflow = containing_block.content.width - total;
+
+        match (width == auto, margin_left == auto, margin_right == auto) {
+            (false, false, false) => {
+                margin_right = Value::Length(margin_right.to_px() + underflow, Unit::Px);
+            }
+
+            (false, false, true) => {
+                margin_right = Value::Length(underflow, Unit::Px);
+            }
+            (false, true, false) => {
+                margin_left = Value::Length(underflow, Unit::Px);
+            }
+
+            (true, _, _) => {
+                if margin_left == auto {
+                    margin_left = Value::Length(0.0, Unit::Px);
+                }
+                if margin_right == auto {
+                    margin_right = Value::Length(0.0, Unit::Px);
+                }
+
+                if underflow >= 0.0 {
+                    width = Value::Length(underflow, Unit::Px);
+                } else {
+                    width = Value::Length(0.0, Unit::Px);
+                    margin_right = Value::Length(margin_right.to_px() + underflow, Unit::Px);
+                }
+            }
+
+            (false, true, true) => {
+                margin_left = Value::Length(underflow / 2.0, Unit::Px);
+                margin_right = Value::Length(underflow / 2.0, Unit::Px);
+            }
+        }
+
+        let d = &mut self.dimensions;
+        d.content.width = width.to_px();
+
+        d.padding.left = padding_left.to_px();
+        d.padding.right = padding_right.to_px();
+
+        d.border.left = border_left.to_px();
+        d.border.right = border_right.to_px();
+
+        d.margin.left = margin_left.to_px();
+        d.margin.right = margin_right.to_px();
+    }
+
+    fn caluclate_block_position(&mut self, containing_block: Dimensions) {
+        let style = self.get_style_node();
+        let d = &mut self.dimensions;
+
+        let zero = Value::Length(0.0, Unit::Px);
+
+        d.margin.top = style.lookup("margin-top", "margin", &zero).to_px();
+        d.margin.bottom = style.lookup("margin-bottom", "margin", &zero).to_px();
+
+        d.border.top = style
+            .lookup("border-top-width", "border-width", &zero)
+            .to_px();
+        d.border.bottom = style
+            .lookup("border-bottom-width", "border-width", &zero)
+            .to_px();
+
+        d.padding.top = style.lookup("padding-top", "padding", &zero).to_px();
+        d.padding.bottom = style.lookup("padding-bottom", "padding", &zero).to_px();
+
+        d.content.x = containing_block.content.x + d.margin.left + d.border.left + d.padding.left;
+
+        d.content.y = containing_block.content.height
+            + containing_block.content.y
+            + d.margin.top
+            + d.border.top
+            + d.padding.top;
+    }
+
+    fn layout_block_children(&mut self) {
+        let d = &mut self.dimensions;
+        for child in &mut self.children {
+            child.layout(*d);
+            d.content.height = d.content.height + child.dimensions.margin_box().height;
+        }
+    }
+
+    fn calculate_block_height(&mut self) {
+        if let Some(Value::Length(h, Unit::Px)) = self.get_style_node().value("height") {
+            self.dimensions.content.height = h;
+        }
+    }
+
+    fn get_style_node(&self) -> &'a StyledNode<'a> {
+        match self.box_type {
+            BoxType::BlockNode(node) | BoxType::InlineNode(node) => node,
+            BoxType::AnonymousBlock => panic!("Anonymous block box has no style node"),
         }
     }
 
